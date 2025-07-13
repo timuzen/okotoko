18fde909b471ea0f0ba43d2b463f27a5c79d410b
timuzen <atr@list.ru>
2025-07-13 20:19:48 +0500

    bugfix

--- handlers/handle_cookie_upload.py
+++ handlers/handle_cookie_upload.py
@@ -2,6 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import ContextTypes
 
+COOKIE_FILENAME = "cookies.txt"
 COOKIE_PATH = "/tmp/cookies.txt"
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
 
@@ -15,12 +16,12 @@ async def handle_cookie_upload(update: Update, context: ContextTypes.DEFAULT_TYP
     document = update.message.document
 
     if not document or document.file_name != COOKIE_FILENAME:
-        await update.message.reply_text("ем только свежие печеньки)")
+        await update.message.reply_text("только печеньки")
         return
 
     try:
         file = await context.bot.get_file(document.file_id)
         await file.download_to_drive(COOKIE_PATH)
-        await update.message.reply_text("ом-ном-ном... печенька съедена)")
+        await update.message.reply_text("печенька съедена)")
     except Exception as e:
         await update.message.reply_text(f"Упс!\n\n {e}")
